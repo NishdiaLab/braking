ddb7f71a5a980d90565be8eb1a52ac02a2cbe894
NishdiaLab <NishdiaLab@gmail.com>
2018-01-18 06:09:34 +0000

    Save 2018/1/18/15:09

---
+++ scripts/alert.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import rospy, copy
+from time import sleep
+from pimouse_ros.msg import LightSensorValues
+from std_msgs.msg import UInt16
+
+class Alert():
+    def __init__(self):
+        self.alert = rospy.Publisher('/buzzer',UInt16,queue_size=1)
+
+        self.sensor_values = LightSensorValues()
+        rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
+
+    def callback(self,messages):
+        self.sensor_values = messages
+
+    def sound(self):
+        rate = rospy.Rate(10)
+
+        buz = UInt16()
+        buz = 0
+        while not rospy.is_shutdown():
+                while self.sensor_values.sum_all >= 80:
+                	buz = 0
+                	self.alert.publish(buz)
+                	sleep(0.1)
+			buz = 1200
+			self.alert.publish(buz)
+			sleep(0.1)
+                	if self.sensor_values.sum_all >= 200:
+                		break
+                while self.sensor_values.sum_all >= 200:
+                	buz = 1200
+                	self.alert.publish(buz)
+                	rate.sleep()
+
+                buz = 0
+                self.alert.publish(buz)
+                rate.sleep()
+
+if __name__ == '__main__':
+    rospy.init_node('alert')
+    Alert().sound()
